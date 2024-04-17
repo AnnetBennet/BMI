101e5518f1011360d1fd2da90616c9cd14c89b1c
Lumos maxima <eanechka91@gmail.com>
2024-04-18 00:30:49 +0500

    Changed folders

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    double calculate(double meters, int kilograms) {
+    int calculate(double meters, int kilograms) {
         double result;
         result = kilograms / (meters * meters);
         return (int) result;
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double BMI = service.calculate(1.65, 57);
-        System.out.println("Индекс массы тела=" + BMI);
+        double BMI = service.calculate(1.71, 76);
+        int BMII = (int) BMI;
+        System.out.println("Индекс массы тела=" + BMII);
     }
 }
